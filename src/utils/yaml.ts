1f40e5305d105159447d8204d7f962a7bd620a4b
Isaac Naylor <i.acnaylor@gmail.com>
2026-05-01 11:00:42 +0100

    Added utils to avoid directly using bun

---
+++ src/utils/yaml.ts
@@ -0,0 +1,17 @@
+export async function parseYaml<T>(content: string): Promise<T | null> {
+    return import("obsidian").then(obsidian => {
+        const obj = obsidian.parseYaml(content);
+        if (obj as T) {
+            return obj as T;
+        }
+
+        return null;
+    }).catch(() => import("bun").then(bun => {
+        const obj = bun.YAML.parse(content);
+        if (obj as T) {
+            return obj as T;
+        }
+
+        return null;
+    }).catch(() => null));
+}

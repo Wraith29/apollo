73cb674f4af559a2e7072bb7773b4c01443fce2e
Isaac Naylor <i.acnaylor@gmail.com>
2026-05-18 14:10:19 +0100

    Auto formatting changes

--- src/utils/yaml.ts
+++ src/utils/yaml.ts
@@ -1,17 +1,23 @@
 export async function parseYaml<T>(content: string): Promise<T | null> {
-    return import("obsidian").then(obsidian => {
-        const obj = obsidian.parseYaml(content);
-        if (obj as T) {
-            return obj as T;
-        }
+	return import("obsidian")
+		.then((obsidian) => {
+			const obj = obsidian.parseYaml(content);
+			if (obj as T) {
+				return obj as T;
+			}
 
-        return null;
-    }).catch(() => import("bun").then(bun => {
-        const obj = bun.YAML.parse(content);
-        if (obj as T) {
-            return obj as T;
-        }
+			return null;
+		})
+		.catch(() =>
+			import("bun")
+				.then((bun) => {
+					const obj = bun.YAML.parse(content);
+					if (obj as T) {
+						return obj as T;
+					}
 
-        return null;
-    }).catch(() => null));
+					return null;
+				})
+				.catch(() => null),
+		);
 }

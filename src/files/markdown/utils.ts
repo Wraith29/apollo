43f8293dd2f2302aad9f0a97d1b906b3a6baaab8
Isaac Naylor <i.acnaylor@gmail.com>
2026-05-18 16:27:43 +0100

    Added lots of markdown building utilities

--- src/files/markdown/utils.ts
+++ src/files/markdown/utils.ts
@@ -1,4 +1,15 @@
-import type { Root } from "mdast";
+import type {
+	BlockContent,
+	DefinitionContent,
+	Heading,
+	Link,
+	List,
+	ListItem,
+	Paragraph,
+	PhrasingContent,
+	Root,
+	Text,
+} from "mdast";
 
 export function getIndexOfHeader(tree: Root, header: string): number {
 	return tree.children.findIndex((node) => {
@@ -12,4 +23,64 @@ export function getIndexOfHeader(tree: Root, header: string): number {
 
 		return textChildren.length > 0;
 	});
+}
+
+type Depth = 1 | 2 | 3 | 4 | 5 | 6;
+
+export function buildHeading(title: string, depth: Depth): Heading {
+	return {
+		type: "heading",
+		depth: depth,
+		children: [
+			{
+				type: "text",
+				value: title,
+			},
+		],
+	};
+}
+
+export function buildText(inner: string): Text {
+	return {
+		type: "text",
+		value: inner,
+	};
+}
+
+export function buildLink(
+	url: string,
+	children: PhrasingContent[],
+	title?: string,
+): Link {
+	return {
+		type: "link",
+		url: url,
+		children: children,
+		title: title,
+	};
+}
+
+export function buildParagraph(children: PhrasingContent[]): Paragraph {
+	return {
+		type: "paragraph",
+		children: children,
+	};
+}
+
+export function buildList(items: ListItem[]): List {
+	return {
+		type: "list",
+		spread: false,
+		children: items,
+	};
+}
+
+export function buildListItem(
+	children: (BlockContent | DefinitionContent)[],
+): ListItem {
+	return {
+		type: "listItem",
+		spread: false,
+		children: children,
+	};
 }
